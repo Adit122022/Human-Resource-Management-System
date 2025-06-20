3ccc2573e3f99f1883f91cca732dfb5211611b17
Adit122022 <adity202212@gmail.com>
2025-06-20 11:15:14 +0530

    Update CORS configuration to allow requests from the production URL

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ app.set('views', "./src/views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin:'http://localhost:5173',  credentials: true  }))
+app.use(cors({origin:'https://human-resource-management-system-alpha.vercel.app',  credentials: true  }))
 
 const auth = require('./routes/auth.route');
 const employee = require('./routes/employee.route');
